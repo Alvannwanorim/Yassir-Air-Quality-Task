a6b01ea567c5d7aedb12868225ef1a9ac7181d4b
alvannwanorim <alvannwanorim@gmail.com>
2022-09-09 09:26:41 +0100

    Test: Implemented test for the endpoints

--- src/air-quality/__mocks__/air-quality.service.ts
+++
@@ -1,6 +0,0 @@
-import { airQualityStub } from '../stubs/air-quality.stub';
-
-export const AirQualityService = jest.fn().mockReturnValue({
-	getAirQualityByLatAndLong: jest.fn().mockReturnValue(airQualityStub()),
-	getAirQualityByNearestCity: jest.fn().mockReturnValue(airQualityStub())
-});
